3dd69f0c605a7f56a9c326ac06720e05f31d9f58
jselga <jordi.selga@iescarlesvallbona.cat>
2026-03-16 15:54:17 +0100

    TreeMap Diccionari

---
+++ T3/exemples/ExemplesCollections/src/_16_Coleccions_TreeMap/Diccionari.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package _16_Coleccions_TreeMap;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author jordi
+ */
+public class Diccionari {
+
+    private Map<String, Paraula> paraules;
+
+    public Diccionari() {
+        paraules = new TreeMap<>();
+    }
+
+    public void afegir(Paraula p) {
+
+        if (!paraules.containsKey(p.getNom().toLowerCase())) {
+
+            paraules.put(p.getNom().toLowerCase(), p);
+            System.out.printf("Registre afegit amb èxit\n");
+        } else {
+            System.out.println("Clau repetida no s'afegeix");
+        }
+
+    }
+
+    public void eliminar(String nom) {
+        paraules.remove(nom.toLowerCase());
+    }
+
+    public Paraula cercar(String nom) {
+        return paraules.get(nom.toLowerCase());
+    }
+
+    public void mostrar() {
+        for (Map.Entry<String, Paraula> entry : paraules.entrySet()) {
+            System.out.printf("Clau: %s|", entry.getKey());
+            System.out.println(entry.getValue());
+        }
+    }
+}
---
+++ T3/exemples/ExemplesCollections/src/_16_Coleccions_TreeMap/Principal.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package _16_Coleccions_TreeMap;
+
+/**
+ *
+ * @author jordi
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Diccionari dic = new Diccionari();
+
+        dic.afegir(new Paraula("Casa", "Lloc on viu la gent", "CAT"));
+        dic.afegir(new Paraula("House", "Building for human habitation", "EN"));
+        dic.afegir(new Paraula("Perro", "Animal doméstico", "ES"));
+        dic.afegir(new Paraula("Gato", "Animal doméstico felino", "ES"));
+        dic.afegir(new Paraula("Gato", "Animal doméstico 2", "ES"));
+
+        System.out.println("Buscar:");
+        System.out.println(dic.cercar("casa"));
+
+        System.out.println("\nDiccionari complet:");
+        dic.mostrar();
+    }
+
+}
